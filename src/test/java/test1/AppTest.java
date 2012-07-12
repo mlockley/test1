52cbfd94f6acc25517b6bdbf05c4de739230771f
Matthew Lockley <matthewlockley@mac.com>
2012-07-12 23:18:21 +0100

    adding fixml schema

--- src/test/java/test1/AppTest.java
+++ src/test/java/test1/AppTest.java
@@ -1,34 +1,52 @@
 package test1;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.fixprotocol.fixml_5_0_sp2.ExecutionReportMessageT;
+import org.fixprotocol.fixml_5_0_sp2.FIXML;
+import org.fixprotocol.fixml_5_0_sp2.ObjectFactory;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBElement;
+
+import java.io.StringWriter;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
+public class AppTest {
+    Logger logger = LoggerFactory.getLogger(AppTest.class);
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
+
+    @Test
+    public void testFixML () {
+        ObjectFactory factory = new ObjectFactory();
+        FIXML fixml = factory.createFIXML();
+
+        ExecutionReportMessageT executionReportMessageT = factory.createExecutionReportMessageT();
+        JAXBElement<ExecutionReportMessageT> execRpt = factory.createExecRpt(executionReportMessageT);
+        fixml.setMessage(execRpt);
+
+        String output = serializeUsingJAXB(fixml);
+        logger.info("output = \n{}",output);
+    }
+
+
+    public static <T> String serializeUsingJAXB(T entity) {
+        StringWriter writer = new StringWriter();
+        javax.xml.bind.JAXB.marshal(entity, writer);
+        return writer.toString();
+    }
+
 }
+
+
